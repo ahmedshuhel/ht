0a26b93e3fe145d8dd825ecb03881f6fc9a915fb
Shuhel Ahmed <ahmedshuhel@newscred.com>
2017-12-21 22:14:56 +0600

    test peristance

--- tests/test_task.py
+++ tests/test_task.py
@@ -112,3 +112,47 @@ def test_throw_if_add_time_in_state_other_than_inprogress():
         task.add_time(10, 'desc')
 
     assert str(err.value) == 'Cannot add time for the task in `init` state'
+
+
+def test_persist_task_with_all_attributes():
+    metadata.create_all(engine)
+
+    minutes1 = 10
+    minutes2 = minutes1 + 5
+    desc1 = 'dummy description'
+    desc2 = desc1 + 'dummy description'
+    task = create_task()
+    task.start()
+    task.add_time(minutes=minutes1, description=desc1)
+    task.add_time(minutes=minutes2, description=desc2)
+
+    session = Session()
+    session.add(task)
+
+    # commits the changes to db. Automatically expires the attributs
+    session.commit()
+
+    # refresh/un-expire the attars so that we can access them after the task
+    # get's detached
+    session.refresh(task)
+
+    # detach all objects from the session
+    session.expunge_all()
+
+    session.close()
+    Session.remove()
+
+    qs = Session()
+
+    q_task = qs.query(Task).get(task.id)
+    assert q_task.title == task.title
+    assert len(q_task.times) == 2
+    assert q_task.times[0].minutes == minutes1
+    assert q_task.times[1].minutes == minutes2
+    assert q_task.times[0].description == desc1
+    assert q_task.times[1].description == desc2
+    assert q_task.description == task.description
+
+    qs.commit()
+    qs.close()
+    Session.remove()
