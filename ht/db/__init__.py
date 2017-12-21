380eb0cb6aa4984a7106648f99723053a4431ccc
Shuhel Ahmed <ahmedshuhel@newscred.com>
2017-12-21 22:24:43 +0600

    export metadata

--- ht/db/__init__.py
+++ ht/db/__init__.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-
+from .mapping import metadata
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 
