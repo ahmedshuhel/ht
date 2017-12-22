273b8bc4b1796a6d6e231c6d3110c228ee8f82df
Shuhel Ahmed <ahmedshuhel@newscred.com>
2017-12-22 21:09:22 +0600

    define metadata with db

--- ht/db/__init__.py
+++ ht/db/__init__.py
@@ -1,5 +1,33 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from ht.models.task import Task, Time
+from sqlalchemy import Table, Column,\
+     Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import mapper, relationship
 
-engine = create_engine('sqlite:///:memory:', echo=True)
-Session = scoped_session(sessionmaker(bind=engine))
+from .ht_database import Database
+
+
+db = Database()
+
+task_metadata = Table(
+    'task', db.metadata,
+    Column('id', String, primary_key=True),
+    Column('title', String(100)),
+    Column('description', String(250)),
+    Column('state', String(50)),
+    Column('created_at', DateTime)
+)
+
+time_metadata = Table(
+    'time', db.metadata,
+    Column('id', String, primary_key=True),
+    Column('minutes', Integer),
+    Column('description', String(250)),
+    Column('task_id', Integer, ForeignKey('task.id'))
+)
+
+
+mapper(Task, task_metadata, properties={
+    'times': relationship(Time)
+})
+
+mapper(Time, time_metadata)
